fe426f2e49da0d2a3c98c33b0b1e0fc65351edc7
ManashRaja <mpdmanash@gmail.com>
2016-06-11 21:32:03 +0530

    Change PidError msg to float, Add Alt Hold Pid

--- src/ROS/subscribe_pid_errors.cpp
+++ src/ROS/subscribe_pid_errors.cpp
@@ -10,6 +10,19 @@ void Subscribe_pid_errors::pidErrorsCb(const ark_controls::PidErrorsConstPtr &ms
 {
     if (this->shared_memory->getSharedControl() && this->shared_memory->getOverride())
     {
+        float errorx = msg->dx;
+        float errory = msg->dy;
+        float errorpsi = msg->dpsi;
+        float errorz = msg->dz; 
 
+        
+        // Alt PID
+        if (abs(errorz) > 0.2)
+        {
+            if (errorz < 0) this->t_gui->gui->channel34->setYValue(1340);
+            else if (errorz > 0) this->t_gui->gui->channel34->setYValue(1660);
+        }            
+        else this->t_gui->gui->channel34->setYValue(1500);
+        // End Alt Pid
     }
 }
